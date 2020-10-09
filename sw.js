2f829f29333c6f83e6843a2111ec70128e9f0723
Escher1108 <3095697814@qq.com>
2020-10-09 19:27:28 +0800

    fix js error

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
-/*
- * @Author: Escher1108
- * @Website: https://jkgblog.com
- * @Date: 2020-07-26 21:53:07
- * @LastEditTime: 2020-10-09 19:21:03
- * @LastEditors: Do not edit
- * @FilePath: \HBc:\Users\Escher\Desktop\BoxA\sw.js
- * @Description: 小白学编程
- */
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.c33e24c5.js')};importScripts('conf.js');importScripts('my.js')
+
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.c33e24c5.js')};importScripts('conf.js')
