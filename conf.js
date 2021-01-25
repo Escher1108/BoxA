3a873bebe32aa08f9e5a7dc512d0d370536b0e69
Escher1108 <3095697814@qq.com>
2021-01-25 22:59:20 +0800

    fix err

--- conf.js
+++ conf.js
@@ -1,12 +1,3 @@
-/*
- * @Author: Escher1108
- * @Website: https://jkgblog.com
- * @Date: 2020-07-26 21:53:07
- * @LastEditTime: 2021-01-25 22:48:47
- * @LastEditors: Do not edit
- * @FilePath: \HBc:\Users\Escher\Desktop\BoxA\conf.js
- * @Description: 耶斯莫拉
- */
 jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
@@ -36,7 +27,7 @@ jsproxy_config({
     //   },
     // },
     'mysite': {
-      label: '当前节点(联通)',
+      label: '当前站点(联通)',
       lines: {
         [location.host]: 1,
       }
@@ -74,7 +65,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-   //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
